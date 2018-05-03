c4c7ec210b1874a89ffb87402e04162953d6cb0c
FredericoCL <fred.lessa@gmail.com>
2018-05-02 22:05:23 -0300

    Implementação das ações de preenchimento das grids por cada jogador, com os ícones de X e O (assim como ocorre no jogo da velha no papel), inserido um array bi-dimensional para guardar as posições das jogadas e a pontuação, faltando apenas o controle efetivo das jogadas para definir o campeão.

--- jogo.js
+++ jogo.js
@@ -1,3 +1,6 @@
+var rodada = 1;
+var matriz_jogo = Array(3);
+
 $(document).ready(function(){
 
 	//Função para iniciar o jogo quando clicado o botão iniciar
@@ -18,6 +21,33 @@ $(document).ready(function(){
 		$('#nome_jogador_1').html($('#entrada_nome_jogador_1').val());
 		$('#nome_jogador_2').html($('#entrada_nome_jogador_2').val());
 
+		//Função para habilitar e desabilitar "telas" Página Inicial e Palco do Jogo - Controla visualização
+		$('#pagina_inicial').hide();
+		$('#palco_jogo').show();
 
-	})
+	});
+
+	$('.jogada').click(function(){
+		var id_campo_clicado = this.id;
+		jogada(id_campo_clicado);
+	});
+
+	function jogada(id){
+		var icone = '';
+		var ponto = 0;
+
+		if((rodada % 2) == 1){
+			icone = 'url("imagens/marcacao_1.png")';
+			ponto = -1;
+		} else {
+			icone = 'url("imagens/marcacao_2.png")';
+			ponto = 1;
+		}
+
+		rodada++;
+
+		$('#'+id).css('background-image', icone);
+
+	}
+
 });
